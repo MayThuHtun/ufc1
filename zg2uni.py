a216616e663b048feb582f1f05e196be1142ea56
MayThuHtun <maythutun.ucsmdy@gmail.com>
2018-07-02 10:53:23 -0700

    Second Step

--- zg2uni.py
+++ zg2uni.py
@@ -16,14 +16,79 @@ def convert(input):
     output = output.replace(u"\u108F", u"\u1014")
     output = output.replace(u"\u1012", u"\u1012")
     output = output.replace(u"\u1013", u"\u1013")
+
+    #  yayit
+    output = output.replace(u'\u103B', u'\u103C')
+    output = output.replace(u'\u107E', u'\u103C')
+    output = output.replace(u'\u107F', u'\u103C')
+    output = output.replace(u'\u1080', u'\u103C')
+    output = output.replace(u'\u1081', u'\u103C')
+    output = output.replace(u'\u1082', u'\u103C')
+    output = output.replace(u'\u1083', u'\u103C')
+    output = output.replace(u'\u1084', u'\u103C')
+
+    ##########
+    #  1chaungngin
+    output = output.replace(u'\u1033', u'\u102F')
+
+    #  2chaungngin
+    output = output.replace(u'\u1034', u'\u1030')
+
+    #  yayit
+    output = output.replace(u'\u103B', u'\u103C')
+    output = output.replace(u'\u107E', u'\u103C')
+    output = output.replace(u'\u107F', u'\u103C')
+    output = output.replace(u'\u1080', u'\u103C')
+    output = output.replace(u'\u1081', u'\u103C')
+    output = output.replace(u'\u1082', u'\u103C')
+    output = output.replace(u'\u1083', u'\u103C')
+    output = output.replace(u'\u1084', u'\u103C')
+
+    #  yapin
+    output = output.replace(u'\u103A', u'\u103B')
+
+    #  nya
+    output = output.replace(u'\u106B', u'\u100A')
+
+    #  hahtoe
+    output = output.replace(u'\u103D', u'\u103E')
+    output = output.replace(u'\u107D', u'\u103E')
+    output = output.replace(u'\u1087', u'\u103E')
+
+    #  yakaut
+    output = output.replace(u'\u1090', u'\u101B')
+
+    #  htawonbae
+    output = output.replace(u'\u1092', u'\u100C')
+
+    #  dayinkaut
+    output = output.replace(u'\u106E', u'\u100D')
+
+    #  tatalin
+    output = output.replace(u'\u1097', u'\u100B')
+
+    #  na
+    output = output.replace(u'\u108F', u'\u1014')
+
+    #  athat
+    output = output.replace(u'\u1039', u'\u103A')
+
+    #  waswae
+    #  thagyi
+    output = output.replace(u'\u1086', u'\u103F')
+
+    #  outkamyint
+    output = output.replace(u'\u1094', u'\u1037')
+    output = output.replace(u'\u1095', u'\u1037')
+
+    ##########
+
     ######
 
     output = output.replace(u"[\u103D\u1087]", u"\u103E")
     #  ha
     output = output.replace(u"\u103C", u"\u103D")
     #  wa
-    output = output.replace(u"[\u103B\u107E\u107F\u1080\u1081\u1082\u1083\u1084]", u"\u103C")
-    #  ya yint (ra)
     output = output.replace(u"[\u103A\u107D]", u"\u103B")
     #  ya
 
@@ -35,13 +100,13 @@ def convert(input):
 
     #  Reordering
 
-    output = output.replace(u"(\u1031)?(\u103C)?([\u1000-\u1021])\u1064", u"\u1064\1\2\3")
+    output = re.sub(u"(\u1031)?(\u103C)?([\u1000-\u1021])\u1064", u"\u1064\1\2\3", output)
     #  reordering kinzi
-    output = output.replace(u"(\u1031)?(\u103C)?([\u1000-\u1021])\u108B", u"\u1064\1\2\3\u102D")
+    output = re.sub(u"(\u1031)?(\u103C)?([\u1000-\u1021])\u108B", u"\u1064\1\2\3\u102D", output)
     #  reordering kinzi lgt
-    output = output.replace(u"(\u1031)?(\u103C)?([\u1000-\u1021])\u108C", u"\u1064\1\2\3\u102E",)
+    output = re.sub(u"(\u1031)?(\u103C)?([\u1000-\u1021])\u108C", u"\u1064\1\2\3\u102E", output)
     #  reordering kinzi lgtsk
-    output = output.replace(u"(\u1031)?(\u103C)?([\u1000-\u1021])\u108D", u"\u1064\1\2\3\u1036")
+    output = re.sub(u"(\u1031)?(\u103C)?([\u1000-\u1021])\u108D", u"\u1064\1\2\3\u1036", output)
     #  reordering kinzi ttt
 
     ##
@@ -62,12 +127,7 @@ def convert(input):
     output = output.replace(u"[\u1094\u1095]", u"\u1037")
     #  aukmyint
 
-    #  / Pasint order human error
-    output = output.replace(u"([\u1000-\u1021])([\u102C\u102D\u102E\u1032\u1036]){1,2}([\u1060\u1061\u1062\u1063\u1065\u1066\u1067\u1068\u1069\u1070\u1071\u1072\u1073\u1074\u1075\u1076\u1077\u1078\u1079\u107A\u107B\u107C\u1085])", r"\1\3\2")
-
-
-    #######/
-
+    #######
     output = output.replace(u"\u1064", u"\u1004\u103A\u1039")
 
     output = output.replace(u"\u104E", u"\u104E\u1004\u103A\u1038")
@@ -82,5 +142,35 @@ def convert(input):
     output = output.replace(u"[\u1066\u1067]", u"\u1039\u1006")
     output = output.replace(u"\u1068", u"\u1039\u1007")
     output = output.replace(u"\u1069", u"\u1039\u1008")
+    output = output.replace(u"\u106C", u"\u1039\u100B")
+    output = output.replace(u"\u1070", u"\u1039\u100F")
+    output = output.replace(u"[\u1071\u1072]", u"\u1039\u1010")
+    output = output.replace(u"[\u1073\u1074]", u"\u1039\u1011")
+    output = output.replace(u"\u1075", u"\u1039\u1012")
+    output = output.replace(u"\u1076", u"\u1039\u1013")
+    output = output.replace(u"\u1077", u"\u1039\u1014")
+    output = output.replace(u"\u1078", u"\u1039\u1015")
+    output = output.replace(u"\u1079", u"\u1039\u1016")
+    output = output.replace(u"\u107A", u"\u1039\u1017")
+    output = output.replace(u"\u107B", u"\u1039\u1018")
+    output = output.replace(u"\u107C", u"\u1039\u1019")
+    output = output.replace(u"\u1085", u"\u1039\u101C")
+    output = output.replace(u"\u106D", u"\u1039\u100C")
+
+    output = output.replace(u"\u1091", u"\u100F\u1039\u100D")
+    output = output.replace(u"\u1092", u"\u100B\u1039\u100C")
+    output = output.replace(u"\u1097", u"\u100B\u1039\u100B")
+    output = output.replace(u"\u106E", u"\u100D\u1039\u100D")
+    output = output.replace(u"\u106F", u"\u100E\u1039\u100D")
+
+    ##########
+    output = re.sub(u"(\u103C)([\u1000-\u1021])(\u1039[\u1000-\u1021])?", r"\2\3\1", output)
+    #  reordering ra
+
+    output = re.sub(u"(\u103E)(\u103D)([\u103B\u103C])", r"\3\2\1", output)
+    output = re.sub(u"(\u103E)([\u103B\u103C])", r"\2\1", output)
+
+    output = re.sub(u"(\u103D)([\u103B\u103C])", r"\2\1", output)
+
 
     return output
